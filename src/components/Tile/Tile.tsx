2c92f9b181305d5e45080e8396a8d02273a26eb1
theodore18johnson <contact@kingoverroad.org>
2025-10-24 21:31:45 +0900

    Reapply "feat:use the custom theme"
    This reverts commit 3f844d4b273f1671d19f45731fc94244405a8984.

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
-import StyledTile, { StyledTileProps } from './StyledTile';
-import StyledTileValue from './StyledTileValue';
+import { FC } from "react";
+import StyledTile, { StyledTileProps } from "./StyledTile";
+import StyledTileValue from "./StyledTileValue";
+import { useGameContext } from "../../context/GameContext";
+import { ImageTheme } from "themes/types";
 
 export interface TileProps extends StyledTileProps {
   isNew?: boolean;
@@ -15,12 +17,67 @@ const Tile: FC<TileProps> = ({
   height,
   isNew = false,
   isMerging = false,
-}) => (
-  <StyledTile value={value} x={x} y={y} width={width} height={height}>
-    <StyledTileValue value={value} isNew={isNew} isMerging={isMerging}>
-      {value}
-    </StyledTileValue>
-  </StyledTile>
-);
+}) => {
+  const { selectedTheme } = useGameContext();
+  return (
+    <>
+      {selectedTheme ? (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            display: "flex",
+            justifyContent: "center",
+            transition: "transform 0.15s ease-in-out",
+            width,
+            height,
+            transform: `translate(${x}px, ${y}px)`,
+          }}
+        >
+          <img
+            src={
+              selectedTheme[
+                value as keyof Omit<
+                  ImageTheme,
+                  | "uuid"
+                  | "title"
+                  | "description"
+                  | "visibility"
+                  | "price"
+                  | "numberDisplay"
+                  | "position"
+                  | "numberColor"
+                  | "numberSize"
+                  | "owned"
+                  | "creator_id"
+                >
+              ]?.sm
+            }
+            style={{
+              width: "100%",
+              height: "100%",
+              fontSize: "inherit",
+              display: "flex",
+              textAlign: "center",
+              flexDirection: "column",
+              justifyContent: "center",
+              animationName: isMerging ? "pop" : isNew ? "expand" : "",
+              animationDuration: "0.18s",
+              animationFillMode: "forwards",
+            }}
+            alt="tile"
+          />
+        </div>
+      ) : (
+        <StyledTile value={value} x={x} y={y} width={width} height={height}>
+          <StyledTileValue value={value} isNew={isNew} isMerging={isMerging}>
+            {value}
+          </StyledTileValue>
+        </StyledTile>
+      )}
+    </>
+  );
+};
 
 export default Tile;
